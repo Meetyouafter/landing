8c110556edf126bb9677084d815a4b50b779562c
Meetyouafter <anton_levus@mail.ru>
2023-05-11 00:01:10 +0530

    added form

--- src/js/main.js
+++ src/js/main.js
@@ -5,21 +5,14 @@ wrapper.addEventListener('click', () => {
   console.log(33);
 });
 
-const serializeForm = (formNode) => {
-  const { elements } = formNode;
-  const data = Array.from(elements)
-    .filter((item) => !!item.name)
-    .map((element) => {
-      const { name, value } = element;
-      return { name, value };
-    });
-  console.log(data);
-};
+function serializeForm(formNode) {
+  return new FormData(formNode);
+}
 
 const handleFormSubmit = (e) => {
   e.preventDefault();
-  serializeForm(form);
+  const formData = serializeForm(form);
+  console.log(Object.fromEntries(formData));
 };
 
 form.addEventListener('submit', handleFormSubmit);
-form.addEventListener('click', handleFormSubmit);
